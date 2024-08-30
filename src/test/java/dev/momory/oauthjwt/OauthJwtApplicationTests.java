38de39a9303da4f156bbdb22a4e335f8caeee687
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-08-30 15:57:36 +0900

    기본 설정

---
+++ src/test/java/dev/momory/oauthjwt/OauthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.momory.oauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
